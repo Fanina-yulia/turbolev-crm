2530982e8ca1a20e4de725a5a4035cf82322f7a9
Fanina-yulia <fanina.yulia@gmail.com>
2026-08-14 20:31:20 +0300

    style: load vehicle lookup styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./new-request-wizard.css";
+import "./vehicle-lookup.css";
 
 export const metadata: Metadata = {
   title: "Turbo LEV CRM",
