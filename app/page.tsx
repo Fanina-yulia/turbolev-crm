ec2be4fdc4affabac50cccedf2b04fd98f652e94
Fanina-yulia <fanina.yulia@gmail.com>
2026-08-19 18:07:22 +0300

    Pass initial settings tab from URL

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ type HomePageProps = {
 export default async function HomePage({ searchParams }: HomePageProps) {
   const params = await searchParams;
   const section = Array.isArray(params.section) ? params.section[0] : params.section;
+  const settingsTab = Array.isArray(params.settingsTab) ? params.settingsTab[0] : params.settingsTab;
 
-  return <CrmShell initialSection={section} />;
+  return <CrmShell initialSection={section} initialSettingsTab={settingsTab} />;
 }
