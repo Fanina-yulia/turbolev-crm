3ab6125cef817dbf087f3bab7cf838936ecd7f83
Fanina-yulia <fanina.yulia@gmail.com>
2026-08-21 15:15:42 +0300

    Apply unified communication card visuals

--- app/client-card-drawer.tsx
+++ app/client-card-drawer.tsx
@@ -2,6 +2,7 @@
 
 import { ClientCardDrawer as ClientCardDrawerCore } from "./client-card-drawer-core";
 import { BinotelPhoneCalls } from "./binotel-phone-calls";
+import { CommunicationsCardConsistencyEnhancer } from "./communications-card-consistency-enhancer";
 
 type Props = {
   open: boolean;
@@ -15,6 +16,7 @@ type Props = {
 
 export function ClientCardDrawer(props: Props) {
   return <>
+    <CommunicationsCardConsistencyEnhancer open={props.open} name={props.name} phone={props.phone} channel={props.channel} />
     <ClientCardDrawerCore {...props} />
     {props.open && props.phone ? <BinotelPhoneCalls phone={props.phone} /> : null}
   </>;
