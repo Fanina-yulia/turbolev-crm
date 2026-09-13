69c29ad3725b295d6b71606a85584cce23f799a4
Fanina-yulia <fanina.yulia@gmail.com>
2026-09-13 13:35:51 +0300

    feat: enable automatic client phone matching

--- app/new-request-launcher.tsx
+++ app/new-request-launcher.tsx
@@ -1,9 +1,10 @@
 "use client";
 
+import { NewRequestPhoneAutoMatchEnhancer } from "./new-request-phone-auto-match-enhancer";
 import { NewRequestPricingBridge } from "./new-request-pricing-bridge";
 import { NewRequestStep3Enhancer } from "./new-request-step3-enhancer";
 import { NewRequestWizardV5 } from "./new-request-wizard-v5";
 
 export function NewRequestLauncher({ showButton = true, onOpenChange }: { showButton?: boolean; onOpenChange?: (open: boolean) => void }) {
-  return <><NewRequestWizardV5 showButton={showButton} onOpenChange={onOpenChange}/><NewRequestPricingBridge/><NewRequestStep3Enhancer/></>;
+  return <><NewRequestWizardV5 showButton={showButton} onOpenChange={onOpenChange}/><NewRequestPricingBridge/><NewRequestStep3Enhancer/><NewRequestPhoneAutoMatchEnhancer/></>;
 }
